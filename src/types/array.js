c478195e9ee369c9de147057ca34e110b2735d21
Gleidson Henrique <gleidson_h@hotmail.com>
2021-02-20 23:01:06 -0300

    feat(Array): create alias addLast (push) and removeLast (pop)

--- src/types/array.js
+++ src/types/array.js
@@ -1,13 +1,16 @@
 require('colors')
 const message = require('../message')
 
-const functionsName = ['add', 'remove', 'addFirst', 'removeFirst']
+const functionsName = ['add', 'remove', 'addFirst', 'removeFirst', 'addLast', 'removeLast']
 
 const functions = {
     add: Array.prototype.push,
     remove: Array.prototype.pop,
     addFirst: Array.prototype.unshift,
-    removeFirst: Array.prototype.shift
+    removeFirst: Array.prototype.shift,
+    // aliases
+    addLast: Array.prototype.push, //alias add
+    removeLast: Array.prototype.pop //alias remove
 }
 
 for (const name of functionsName) {
