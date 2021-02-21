2e2d6c61a3e795b832d9ae889551557fd1f55e36
Gleidson Henrique <gleidson_h@hotmail.com>
2021-02-20 23:11:31 -0300

    feat(Array): create prototype isEmpty and isFilled

--- src/types/array.js
+++ src/types/array.js
@@ -1,16 +1,25 @@
-require('colors')
 const message = require('../message')
+require('colors')
+const isEmpty = require('lodash.isempty')
 
-const functionsName = ['add', 'remove', 'addFirst', 'removeFirst', 'addLast', 'removeLast']
+const functionsName = [
+    'add', 'addLast', 'remove', 'removeLast', 'addFirst', 'removeFirst',
+    'isEmpty', 'isFilled'
+]
 
 const functions = {
     add: Array.prototype.push,
+    addLast: Array.prototype.push, //alias add
     remove: Array.prototype.pop,
+    removeLast: Array.prototype.pop, //alias remove
     addFirst: Array.prototype.unshift,
     removeFirst: Array.prototype.shift,
-    // aliases
-    addLast: Array.prototype.push, //alias add
-    removeLast: Array.prototype.pop //alias remove
+    isEmpty: function () {
+        return isEmpty(this)
+    },
+    isFilled: function () {
+        return !isEmpty(this)
+    },
 }
 
 for (const name of functionsName) {
