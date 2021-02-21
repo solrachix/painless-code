58953ba63081d9ff3cfc9778498a6ea5932bec30
Gleidson Henrique <gleidson_h@hotmail.com>
2021-02-20 23:21:54 -0300

    feat(Array): create prototype shuffle

--- src/types/array.js
+++ src/types/array.js
@@ -3,28 +3,44 @@ require('colors')
 const isEmpty = require('lodash.isempty')
 
 const functionsName = [
-    'add', 'addLast', 'remove', 'removeLast', 'addFirst', 'removeFirst',
-    'isEmpty', 'isFilled'
+  'add', 'addLast', 'remove', 'removeLast', 'addFirst', 'removeFirst',
+  'isEmpty', 'isFilled',
+  'shuffle'
 ]
 
 const functions = {
-    add: Array.prototype.push,
-    addLast: Array.prototype.push, //alias add
-    remove: Array.prototype.pop,
-    removeLast: Array.prototype.pop, //alias remove
-    addFirst: Array.prototype.unshift,
-    removeFirst: Array.prototype.shift,
-    isEmpty: function () {
-        return isEmpty(this)
-    },
-    isFilled: function () {
-        return !isEmpty(this)
-    },
+  add: Array.prototype.push,
+  addLast: Array.prototype.push, //alias add
+  remove: Array.prototype.pop,
+  removeLast: Array.prototype.pop, //alias remove
+  addFirst: Array.prototype.unshift,
+  removeFirst: Array.prototype.shift,
+  isEmpty: function () {
+    return isEmpty(this)
+  },
+  isFilled: function () {
+    return !isEmpty(this)
+  },
+  shuffle: function () {
+    let currentIndex = this.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = this[currentIndex];
+      this[currentIndex] = this[randomIndex];
+      this[randomIndex] = temporaryValue;
+    }
+
+    return this
+  }
 }
 
 for (const name of functionsName) {
-    if (typeof Array.prototype[name] !== 'function') Array.prototype[name] = functions[name]
-    else message.exist(name)
+  if (typeof Array.prototype[name] !== 'function') Array.prototype[name] = functions[name]
+  else message.exist(name)
 
-    Array.prototype['_' + name] = functions[name]
+  Array.prototype['_' + name] = functions[name]
 }
